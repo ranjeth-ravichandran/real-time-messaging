58e6c6934cdf1b2aaf18724f7d3e047a364e6066
Ranjeth <ranjeth1965@gmail.com>
2025-05-28 18:06:32 +0100

    fixing wss

--- app/index.js
+++ app/index.js
@@ -1,4 +1,4 @@
-const socket = new WebSocket('ws://real-time-messaging.onrender.com');
+const socket = new WebSocket('wss://real-time-messaging.onrender.com');
 const log = document.getElementById('log');
 const userDropdown = document.getElementById('userSelect');
 
